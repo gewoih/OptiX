0ad2d5032924981e43b52e747cc20eb79db4f3d0
Nikita Ranenko <nranenko@bk.ru>
2024-11-28 14:54:55 +0100

    chore: project structure

--- OptiX.App/Program.cs
+++ OptiX.App/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace OptiX;
+namespace OptiX.App;
 
 public class Program
 {
--- OptiX.App/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
+++ OptiX.App/wwwroot/lib/bootstrap/dist/js/bootstrap.bundle.js
@@ -1128,7 +1128,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5248,
+    interval: 5181,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -6137,7 +6137,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5248
+    delay: 5181
   };
 
   /**
