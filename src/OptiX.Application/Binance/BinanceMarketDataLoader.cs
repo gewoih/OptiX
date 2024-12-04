398b4ead07185f919f7c9fef67a9bca1ea8c4dd0
Nikita Ranenko <nranenko@bk.ru>
2024-12-04 18:10:26 +0100

    feat: BinanceMarketDataLoader.cs reworked and optimized

--- src/OptiX.Application/Binance/BinanceMarketDataLoader.cs
+++ src/OptiX.Application/Binance/BinanceMarketDataLoader.cs
@@ -1,4 +1,4 @@
-using System.Collections.Concurrent;
+using System.Threading.Channels;
 using Binance.Net.Clients;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,8 +13,7 @@ namespace OptiX.Application.Binance;
 public sealed class BinanceMarketDataLoader : BackgroundService
 {
     private readonly BinanceSocketClient _binanceSocketClient;
-    private readonly DateTime _ticksLastSavedAt;
-    private readonly ConcurrentBag<TickDto> _ticksToSave;
+    private readonly Channel<TickDto> _tickChannel;
     private readonly IHubContext<MarketDataHub> _hubContext;
     private readonly IServiceProvider _serviceProvider;
 
@@ -23,47 +22,99 @@ public sealed class BinanceMarketDataLoader : BackgroundService
         _binanceSocketClient = new BinanceSocketClient();
         _serviceProvider = serviceProvider;
         _hubContext = hubContext;
-        _ticksToSave = [];
-        _ticksLastSavedAt = DateTime.UtcNow;
+        _tickChannel = Channel.CreateUnbounded<TickDto>();
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var saveTicksTask = SaveTicksAsync(stoppingToken);
+        var broadcastTicksTask = BroadcastTicksAsync(stoppingToken);
+
         using var scope = _serviceProvider.CreateScope();
         var assetService = scope.ServiceProvider.GetRequiredService<IAssetService>();
-        
+
         var assets = await assetService.GetAllAsync();
-        var symbolsToSubscribe = assets.Select(asset => asset.Symbol);
+        var symbolsToSubscribe = assets.Select(asset => asset.Symbol).ToList();
         var symbolsAndAssetIds = assets.ToDictionary(key => key.Symbol, value => value.Id);
 
         if (!symbolsAndAssetIds.Any())
             return;
-        
-        var tickerSubscriptionResult = await _binanceSocketClient.SpotApi.ExchangeData.SubscribeToTradeUpdatesAsync(
+
+        var subscriptionResult = await _binanceSocketClient.SpotApi.ExchangeData.SubscribeToTradeUpdatesAsync(
             symbolsToSubscribe, async update =>
             {
-                using var scope = _serviceProvider.CreateScope();
-                var ticksService = scope.ServiceProvider.GetRequiredService<ITickService>();
+                if (string.IsNullOrEmpty(update.Symbol))
+                    return;
+
+                if (!symbolsAndAssetIds.TryGetValue(update.Symbol, out var assetId)) 
+                    return;
                 
-                var assetId = symbolsAndAssetIds[update.Symbol];
                 var tick = new TickDto
                 {
                     AssetId = assetId,
                     DateTime = update.Data.TradeTime,
                     Volume = update.Data.Quantity,
-                    Price = update.Data.Price
+                    Price = update.Data.Price,
                 };
-                
-                _ticksToSave.Add(tick);
 
-                var timeSinceLastSaved = DateTime.UtcNow - _ticksLastSavedAt;
-                if (timeSinceLastSaved >= TimeSpan.FromSeconds(1))
-                {
-                    await ticksService.SaveAsync(_ticksToSave);
-                    _ticksToSave.Clear();
-                }
-                
-                await _hubContext.Clients.Group(update.Symbol).SendAsync("ReceiveMarketData", tick, cancellationToken: stoppingToken);
+                _tickChannel.Writer.TryWrite(tick);
             }, stoppingToken);
+
+        await Task.WhenAll(saveTicksTask, broadcastTicksTask);
+    }
+
+    private async Task SaveTicksAsync(CancellationToken stoppingToken)
+    {
+        var ticksBuffer = new List<TickDto>();
+        var flushInterval = TimeSpan.FromSeconds(1);
+        var nextFlush = DateTime.UtcNow.Add(flushInterval);
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            while (await _tickChannel.Reader.WaitToReadAsync(stoppingToken))
+            {
+                while (_tickChannel.Reader.TryRead(out var tick))
+                {
+                    ticksBuffer.Add(tick);
+
+                    if (DateTime.UtcNow < nextFlush)
+                        continue;
+
+                    using var scope = _serviceProvider.CreateScope();
+                    var ticksService = scope.ServiceProvider.GetRequiredService<ITickService>();
+
+                    try
+                    {
+                        await ticksService.SaveAsync(ticksBuffer);
+                    }
+                    catch (Exception ex)
+                    {
+                        // ignored
+                    }
+
+                    ticksBuffer.Clear();
+                    nextFlush = DateTime.UtcNow.Add(flushInterval);
+                }
+            }
+        }
+    }
+
+    private async Task BroadcastTicksAsync(CancellationToken stoppingToken)
+    {
+        while (await _tickChannel.Reader.WaitToReadAsync(stoppingToken))
+        {
+            while (_tickChannel.Reader.TryRead(out var tick))
+            {
+                try
+                {
+                    await _hubContext.Clients.Group(tick.AssetId.ToString())
+                        .SendAsync("ReceiveMarketData", tick, cancellationToken: stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions (e.g., log them)
+                }
+            }
+        }
     }
 }
